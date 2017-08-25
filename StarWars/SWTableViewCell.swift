c91aaea277f34493a28b598b9450e4e3da0482b3
Jim Wilson <jamesmattheywilson@gmail.com>
2017-08-25 11:01:11 -0400

    TableView setup

---
+++ StarWars/SWTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SWTableViewCell.swift
+//  StarWars
+//
+//  Created by James Wilson on 8/25/17.
+//  Copyright © 2017 jimdanger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SWTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var swlabel: UILabel!
+
+
+
+}
--- StarWars/ViewController.swift
+++ StarWars/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
 
     @IBOutlet weak var openingCrawl: UILabel!
 
+    @IBOutlet weak var tableView: UITableView!
+
+    var array: [String]? = ["asdf","qwer","xfc","dfgh"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
+        setupTableView()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    func setupTableView(){
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     func loadData() {
@@ -32,14 +42,44 @@ class ViewController: UIViewController {
             self.processFilm(film: film)
         }
 
-
+         array = ["asdf","qwer","xfc","dfgh"]
     }
 
     func processFilm(film: Film){
 
-//        title.text = film.title
+
         openingCrawl.text = film.opening_crawl
     }
+
+
+    // MARK: - UITableViewDataSource
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "swcell", for: indexPath as IndexPath) as! SWTableViewCell
+
+        if let unwrappedArray = array {
+            cell.swlabel.text = unwrappedArray[indexPath.row]
+        }
+
+        return cell
+
+    }
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+
+        if let unwrappedArray = array {
+            return unwrappedArray.count
+        } else {
+            return 0
+        }
+
+
+    }
+
+    
+
 
 
 }
